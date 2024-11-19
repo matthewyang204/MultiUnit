089ddd1a1c5563e45487b28d1d88977334f3aae5
matthewyang204 <141765903+matthewyang204@users.noreply.github.com>
2024-11-18 22:25:25 -0800

    Start writing main menu code and import CFM LFM converter code over

--- src/MultiUnit.py
+++ src/MultiUnit.py
@@ -6,7 +6,25 @@ def splash():
     print("Copyright Matthew Yang 2024-2025")
     print("--------------------------------")
 
+def mainmenu():
+    print("What type of unit do you want to convert?")
 
+def cfmlfm(x):
+    if x == 1:
+        cfm = input("Enter your CFM here: ")
+        cfmint = int(cfm)
+        area = input("Enter your area (ft^2): ")
+        areaint = int(area)
+        lfm = cfmint/areaint
+        print("LFM = " + str(lfm))
+    
+    elif x == 2:
+        lfm = input("Enter your LFM here: ")
+        lfmint = int(lfm)
+        area = input("Enter your area here:")
+        areaint = int(area)
+        cfm = lfmint * areaint
+        print("CFM = " + str(cfm))
 
 # Display the welcome splash screen
 splash()
