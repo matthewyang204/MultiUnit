cf073972a3f9b352a1685270788b9e1a169529ab
matthewyang204 <141765903+matthewyang204@users.noreply.github.com>
2024-11-18 22:09:30 -0800

    Create MultiUnit.py

---
+++ src/MultiUnit.py
@@ -0,0 +1,12 @@
+# Define all functions required to run program at first
+def splash():
+    print("------------")
+    print("MultiUnit v1")
+    print("--------------------------------")
+    print("Copyright Matthew Yang 2024-2025")
+    print("--------------------------------")
+
+
+
+# Display the welcome splash screen
+splash()
