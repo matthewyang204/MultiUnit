1af986c4125f4e2f6836a06a9510f56a395c0b1a
Matthew Yang <matthew.yang204@gmail.com>
2025-08-14 07:16:11 -0700

    Make the unit converter start at a minimal size, fix anchoring problems, etc

--- src/MultiUnit/Form1.Designer.vb
+++ src/MultiUnit/Form1.Designer.vb
@@ -20,7 +20,7 @@ Partial Class MultiUnit
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Input = New System.Windows.Forms.TextBox()
         Me.inputmessage = New System.Windows.Forms.Label()
@@ -36,134 +36,116 @@ Partial Class MultiUnit
         'Input
         '
         Me.Input.AccessibleName = "input"
-        Me.Input.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Input.Anchor = System.Windows.Forms.AnchorStyles.Top
         Me.Input.Font = New System.Drawing.Font("Calibri", 15.0!)
-        Me.Input.Location = New System.Drawing.Point(319, 175)
-        Me.Input.Margin = New System.Windows.Forms.Padding(2)
+        Me.Input.Location = New System.Drawing.Point(123, 249)
+        Me.Input.Margin = New System.Windows.Forms.Padding(4)
         Me.Input.Name = "Input"
-        Me.Input.Size = New System.Drawing.Size(260, 32)
+        Me.Input.Size = New System.Drawing.Size(516, 56)
         Me.Input.TabIndex = 0
         '
         'inputmessage
         '
-        Me.inputmessage.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.inputmessage.Anchor = System.Windows.Forms.AnchorStyles.Top
         Me.inputmessage.AutoSize = True
         Me.inputmessage.Font = New System.Drawing.Font("Calibri", 12.0!)
-        Me.inputmessage.Location = New System.Drawing.Point(328, 140)
-        Me.inputmessage.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.inputmessage.Location = New System.Drawing.Point(135, 174)
+        Me.inputmessage.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.inputmessage.Name = "inputmessage"
-        Me.inputmessage.Size = New System.Drawing.Size(243, 19)
+        Me.inputmessage.Size = New System.Drawing.Size(493, 39)
         Me.inputmessage.TabIndex = 1
         Me.inputmessage.Text = "Enter the value you want to convert:"
         '
         'UnitCategorySelector
         '
-        Me.UnitCategorySelector.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.UnitCategorySelector.Anchor = System.Windows.Forms.AnchorStyles.Top
         Me.UnitCategorySelector.Cursor = System.Windows.Forms.Cursors.Default
         Me.UnitCategorySelector.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.UnitCategorySelector.Font = New System.Drawing.Font("Calibri", 15.0!)
         Me.UnitCategorySelector.FormattingEnabled = True
         Me.UnitCategorySelector.Items.AddRange(New Object() {"Temperature", "Length", "Speed", "Weight", "Mass", "Air Flow"})
-        Me.UnitCategorySelector.Location = New System.Drawing.Point(366, 254)
-        Me.UnitCategorySelector.Margin = New System.Windows.Forms.Padding(2)
+        Me.UnitCategorySelector.Location = New System.Drawing.Point(217, 416)
+        Me.UnitCategorySelector.Margin = New System.Windows.Forms.Padding(4)
         Me.UnitCategorySelector.Name = "UnitCategorySelector"
-        Me.UnitCategorySelector.Size = New System.Drawing.Size(166, 32)
+        Me.UnitCategorySelector.Size = New System.Drawing.Size(328, 57)
         Me.UnitCategorySelector.TabIndex = 2
         '
         'UnitSelectionBox
         '
-        Me.UnitSelectionBox.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.UnitSelectionBox.Anchor = System.Windows.Forms.AnchorStyles.Top
         Me.UnitSelectionBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.UnitSelectionBox.Font = New System.Drawing.Font("Calibri", 15.0!)
         Me.UnitSelectionBox.FormattingEnabled = True
-        Me.UnitSelectionBox.Location = New System.Drawing.Point(341, 333)
-        Me.UnitSelectionBox.Margin = New System.Windows.Forms.Padding(2)
+        Me.UnitSelectionBox.Location = New System.Drawing.Point(167, 584)
+        Me.UnitSelectionBox.Margin = New System.Windows.Forms.Padding(4)
         Me.UnitSelectionBox.Name = "UnitSelectionBox"
-        Me.UnitSelectionBox.Size = New System.Drawing.Size(216, 32)
+        Me.UnitSelectionBox.Size = New System.Drawing.Size(428, 57)
         Me.UnitSelectionBox.TabIndex = 3
         '
         'Label1
         '
-        Me.Label1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.Anchor = System.Windows.Forms.AnchorStyles.Top
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Calibri", 12.0!)
-        Me.Label1.Location = New System.Drawing.Point(268, 219)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Location = New System.Drawing.Point(14, 341)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(363, 19)
+        Me.Label1.Size = New System.Drawing.Size(735, 39)
         Me.Label1.TabIndex = 4
         Me.Label1.Text = "Select the category of conversion you want to perform:"
         '
         'Label2
         '
-        Me.Label2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label2.Anchor = System.Windows.Forms.AnchorStyles.Top
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Calibri", 12.0!)
-        Me.Label2.Location = New System.Drawing.Point(306, 298)
-        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Location = New System.Drawing.Point(91, 509)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(286, 19)
+        Me.Label2.Size = New System.Drawing.Size(581, 39)
         Me.Label2.TabIndex = 5
         Me.Label2.Text = "Select the conversion you want to perform:"
         '
         'ResultBox
         '
-        Me.ResultBox.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ResultBox.Anchor = System.Windows.Forms.AnchorStyles.Top
         Me.ResultBox.Font = New System.Drawing.Font("Calibri", 15.0!)
-        Me.ResultBox.Location = New System.Drawing.Point(280, 451)
-        Me.ResultBox.Margin = New System.Windows.Forms.Padding(2)
+        Me.ResultBox.Location = New System.Drawing.Point(45, 770)
+        Me.ResultBox.Margin = New System.Windows.Forms.Padding(4)
         Me.ResultBox.Name = "ResultBox"
         Me.ResultBox.ReadOnly = True
-        Me.ResultBox.Size = New System.Drawing.Size(338, 32)
+        Me.ResultBox.Size = New System.Drawing.Size(672, 56)
         Me.ResultBox.TabIndex = 6
         '
         'Label3
         '
-        Me.Label3.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label3.Anchor = System.Windows.Forms.AnchorStyles.Top
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Calibri", 12.0!)
-        Me.Label3.Location = New System.Drawing.Point(422, 416)
-        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Location = New System.Drawing.Point(328, 727)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(54, 19)
+        Me.Label3.Size = New System.Drawing.Size(106, 39)
         Me.Label3.TabIndex = 7
         Me.Label3.Text = "Result:"
         '
         'ConvertButton
         '
-        Me.ConvertButton.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ConvertButton.Anchor = System.Windows.Forms.AnchorStyles.Top
         Me.ConvertButton.Font = New System.Drawing.Font("Calibri", 12.0!)
-        Me.ConvertButton.Location = New System.Drawing.Point(361, 377)
-        Me.ConvertButton.Margin = New System.Windows.Forms.Padding(2)
+        Me.ConvertButton.Location = New System.Drawing.Point(175, 649)
+        Me.ConvertButton.Margin = New System.Windows.Forms.Padding(4)
         Me.ConvertButton.Name = "ConvertButton"
-        Me.ConvertButton.Size = New System.Drawing.Size(176, 24)
+        Me.ConvertButton.Size = New System.Drawing.Size(412, 57)
         Me.ConvertButton.TabIndex = 8
         Me.ConvertButton.Text = "Convert"
         Me.ConvertButton.UseVisualStyleBackColor = True
         '
         'MultiUnit
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(898, 497)
+        Me.ClientSize = New System.Drawing.Size(763, 1000)
         Me.Controls.Add(Me.ConvertButton)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.ResultBox)
@@ -173,7 +155,7 @@ Partial Class MultiUnit
         Me.Controls.Add(Me.UnitCategorySelector)
         Me.Controls.Add(Me.inputmessage)
         Me.Controls.Add(Me.Input)
-        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "MultiUnit"
         Me.Text = "MultiUnit"
         Me.ResumeLayout(False)
