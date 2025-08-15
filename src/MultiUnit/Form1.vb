808c3536539e0c1119d770d6ac6fca20db6bcd95
Matthew Yang <matthew.yang204@gmail.com>
2025-08-15 09:52:20 -0700

    Add label4 click sub (useless)

--- src/MultiUnit/Form1.vb
+++ src/MultiUnit/Form1.vb
@@ -334,5 +334,9 @@
     Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
 
     End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
 End Class
 
