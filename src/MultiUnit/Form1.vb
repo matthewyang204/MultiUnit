0d12bcfc6155549386f176fb644184f8db1ddcf6
Matthew Yang <matthew.yang204@gmail.com>
2025-08-18 06:49:47 -0700

    Don't define it as it's useless

--- src/MultiUnit/Form1.vb
+++ src/MultiUnit/Form1.vb
@@ -332,7 +332,6 @@
             MessageBox.Show("Invalid input. Please enter a valid number.")
             Exit Sub
         End Try
-        Dim calcTemp As Double
         Dim category As String = UnitCategorySelector.SelectedItem.ToString()
         Dim fromUnit As String = UnitSelectionBox.SelectedItem.ToString()
         Dim toUnit As String = Unit2SelectionBox.SelectedItem.ToString()
